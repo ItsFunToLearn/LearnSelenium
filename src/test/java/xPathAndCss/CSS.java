c3b4c3c8bfa7070ddb123d737a1446f6ae6f1dea
rathnatestingRathna-omkar <learning.loa@gmail.com>
2017-08-25 17:22:21 +0530

    Initial code

---
+++ src/test/java/xPathAndCss/CSS.java
@@ -0,0 +1,35 @@
+package xPathAndCss;
+
+public class CSS {
+/*
+ * 
+#       	id
+. 			class name
+* 			contains
+^ 			starts-with
+$ 			ends-with
++ 			sibling (elements at same level)
+> 			go to child
+>:first-child
+>:last-child
+>:nth-child()
+attribute	tag-name[attribute='value']
+multiple attributes  tag-name[attribute1='value1'][attribute2='value2']
+
+[title='Recover your forgotten password']
+a[title='Recover your forgotten password']
+[title^='Recover your']              							//starts-with
+a[title$='forgotten password']       							//ends-with
+a[title*='forgotten password']       							//contains
+#day+select+select 												//sibling [facebook]
+span[data-name="gender_wrapper"]>:nth-child(1)>:nth-child(2)    //nth-child
+span[data-name="gender_wrapper"]>:first-child>:last-child       //first child and last child
+
+
+
+
+
+
+
+*/
+}
---
+++ src/test/java/xPathAndCss/Xpath.java
@@ -0,0 +1,63 @@
+package xPathAndCss;
+
+public class Xpath {
+	
+	/*
+	IDENTIFYING WEB ELEMENT USING MULTIPLE ATTRIBUTES
+	 input[@type='text' and @name='cars']
+	 
+	WHEN SAME WEB ELEMENT APPEARS MULTIPLE TIMES IN UI - IDENTIFY USING PARENT
+	1 div[@class='rb']/input[@value='Buy Now']
+	 parent -->     div[@class='rb']
+	 child  -->		input[@value='Buy Now']
+	 
+	2 //tr[td[a[@href='http://msg0.com']]]/td[1]/input
+	 
+	 WORKING WITH SINGLE WEBTABLE
+	 1st row 1st column 
+	 //tr[1]/td[1]/a[@href='http://www.gmail.com']
+	  
+	 WORKING WITH MULTIPLE WEB TABLES
+	 //table[@name='tab1']/tbody/tr[1]/td[2]/a
+	 
+	 XPATH FUNCTIONS
+	 
+	 text()
+	 //h1[text()='Flipkart']
+	 //h1[@class='header' and text()='Welcome to 2.2 million users' ]
+	 
+	 normalize-space()
+	 //h2[normalize-space(text)='All brands']
+	 //input[normalize-space(@value)='show']
+	 //input[@id='name' and normalize-space(@placeholder)='Enter your name']
+	 
+	 contains()
+	 //h1[contains(text, 'welcome to')]
+	 //input[contains(@value, 'Rs')]
+
+	 Following Sibling
+	 //htmlTag/following-sibling::htmlTag
+	 //img[@src='image3.png']/following-sibling::input[@type='button'] 
+	 
+	 Preceeding-sibling
+	 //htmlTag/preceeding-sibling::htmlTag
+	 //span[text()='Rs 1 - Rs 100']/preceeding-sibling::input[@type='checkbox']
+	 
+	 descendant
+	 //htmltag/descendant::htmltag or //htmltag//htmltag
+	 
+	 Using Starts-With to find the elements:
+	 Syntax: //tag[starts-with(attribute,‘value’)]
+	 //legend[starts-with(text(), 'Radio Button Example')]
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 */
+
+}
